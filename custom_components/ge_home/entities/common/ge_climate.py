072ab44df67e839ff1d508f8ff663dcf293bd44d
Steve2Go <steve@grigg.me>
2024-03-31 18:59:36 +1000

    Workaround for HA Rounding on target temp
    HA rounds incorrectly giving numbers for target temp such as 23.9, 24.4 etc, which doesn't match the display on the AC.
    
    This converts back and forth to make the numbers match & make sense.

--- custom_components/ge_home/entities/common/ge_climate.py
+++ custom_components/ge_home/entities/common/ge_climate.py
@@ -94,7 +94,12 @@ class GeClimate(GeEntity, ClimateEntity):
 
     @property
     def target_temperature(self) -> Optional[float]:
-        return float(self.appliance.get_erd_value(self.target_temperature_erd_code))
+        measurement_system = self.appliance.get_erd_value(ErdCode.TEMPERATURE_UNIT)
+        if measurement_system == ErdMeasurementUnits.METRIC:
+            targ = float(self.appliance.get_erd_value(self.target_temperature_erd_code))
+            targ = round( ((targ - 32.0) * (5/9)) / 2 ) * 2 
+            return (9 * targ) / 5 + 32
+        return float(self.appliance.get_erd_value(self.target_temperature_erd_code)
 
     @property
     def current_temperature(self) -> Optional[float]:
